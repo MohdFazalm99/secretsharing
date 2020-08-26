6b15fe344ca9d972340c457683f835509a7addec
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2020-08-26 14:09:09 +0530

    level 2 encryption has been embeded in database

--- app.js
+++ app.js
@@ -2,12 +2,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app= express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema=new mongoose.Schema({
+  email:String,
+  password:String
+});
+
+const secret= "Fazal is not motu";
+userSchema.plugin(encrypt, { secret: secret,encryptedFields: ['password']  }); //here we use it before we create mongoose model
+
+const User= new mongoose.model("User", userSchema);
 
 
 app.get("/", function(req,res){
@@ -22,7 +36,36 @@ app.get("/register", function(req,res){
   res.render("register");
 });
 
+app.post("/register", function(req,res){
+  const newUser= new User({
+    email:req.body.username,
+    password:req.body.password
+  });
+  newUser.save(function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets")
+    }
+  });
+});
 
+app.post("/login",function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email:username}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
 
 
 
