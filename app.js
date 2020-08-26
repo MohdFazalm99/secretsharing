f2019ee8fd97cba2282b61acf352aef05c1eeb26
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2020-08-26 15:20:48 +0530

    Environment variable has been used to Keep Secret safe

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -6,6 +7,8 @@ const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 
 const app= express();
+
+console.log(process.env.SECRET);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -18,8 +21,8 @@ const userSchema=new mongoose.Schema({
   password:String
 });
 
-const secret= "Fazal is not motu";
-userSchema.plugin(encrypt, { secret: secret,encryptedFields: ['password']  }); //here we use it before we create mongoose model
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ['password']  }); //here we use it before we create mongoose model
 
 const User= new mongoose.model("User", userSchema);
 
